4655442aa05aab991cc736eb4559f8ee393acfd3
alm35943 <malek.alsibai@st.oth-regensburg.de>
2025-05-15 20:46:28 +0200

    created the project according to t-s structure. Configured the docker files as well and removed the health check to work during development

---
+++ relay-backend/src/test/java/oth/ics/wtp/relaybackend/RelayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package oth.ics.wtp.relaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
