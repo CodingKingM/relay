42a5e9d8766fbefc81b1e20c50fee350a72dfda5
Malek Alsibai <malek.alsibai@st.oth-regensburg.de>
2025-07-06 23:53:46 +0200

    finally fixed post deletion by using native SQL with correct post_id for likes/comments and all tests pass and deletion works in app

--- relay-backend/src/main/java/oth/ics/wtp/relaybackend/repositories/CommentRepository.java
+++ relay-backend/src/main/java/oth/ics/wtp/relaybackend/repositories/CommentRepository.java
@@ -1,10 +1,17 @@
 package oth.ics.wtp.relaybackend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import oth.ics.wtp.relaybackend.entities.Comment;
 import oth.ics.wtp.relaybackend.entities.Post;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPostOrderByCreatedAtAsc(Post post);
+    
+    @Modifying
+    @Query("DELETE FROM Comment c WHERE c.post = :post")
+    void deleteByPost(@Param("post") Post post);
 } 
